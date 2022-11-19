1c1ec0a08d3e24e8ea2c3568078d5f66ea63f4c6
Oltea <oltea1907@gmail.com>
2022-11-19 22:20:12 +0000

    just pull this one

--- badfaith/src/App.js
+++ badfaith/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         WE need good score on this
         </p>
         <a
           className="App-link"
