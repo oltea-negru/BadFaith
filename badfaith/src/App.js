f1acf4a73238a14188128db4b1aff6f24af1a2df
Oltea <oltea1907@gmail.com>
2022-11-19 22:43:34 +0000

    skeleton done

--- badfaith/src/App.js
+++ badfaith/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import First from "./pages/First";
+import Second from "./pages/Second";
+import
+{
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         WE need good score on this
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+				<Routes>
+					<Route path="/" element={<First/>} />
+					<Route path="/second" element={<Second/>} />
+				</Routes>
+		</Router>
   );
 }
 
