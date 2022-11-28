11eab1cee3b4632aaaa3b62b87b64e460a6efff6
Gavin Aren <gavinaren02@gmail.com>
2022-11-28 22:23:02 +0000

    Test file for signup

--- backend/azure_functions/signup_tests/signup_test.py
+++ backend/azure_functions/signup_tests/signup_test.py
@@ -16,42 +16,76 @@ class TestFunction(unittest.TestCase):
     def test_register_player(self):
 
         # Json for testing username
-        
-        checkValidUname = {"username": "MrDEEP" , "password" : "spec4356#?"}
+        # Checking for a short username
+        input1 = {
+            'username' : "MrD",
+            "password" : "spec4356#?"
+        }
+
+        # Checking for a long username
+        input2 = {
+                'username' : "MrDEEPPPPPPP",
+                "password" : "spec4356#?"
+            }
+
+        # Checking for a short password
+        input3 = {
+                'username' : "MrDEEP",
+                "password" : "6#?"
+            }
+
+        # Checking for a long password
+        input4 = {
+                'username' : "MrDEEP",
+                "password" : "PasswordSoLongThatItIsOverThe30CharLimit"
+            }
+
+        #Checking for a valid signup input and duplicate password
+        input5 = {
+                'username' : "MrDEEP2",
+                "password" : "password2"
+            }
+
+        #invalid dictionary format
+        input6 = {
+                "password" : "spec4356#?"
+            }
+
+        #invalid dictionary format
+        input7 = {
+                'username' : "Mr DEEP"
+            }
 
         
         resp = requests.get (
                #'https://comp3207cw1-ap2g20.azurewebsites.net/api/registerplayer' ,
                'http://localhost:7071/api/SignUp'   ,                   
                                                                         
-                json = checkValidUname
+                json = input5
          )
 
-        all_users = list(self.player_container.read_all_items())
-     # Checking for a short username
-    #    self.assertEqual(resp.json()["msg"], "Username less than 4 characters or more than 16 characters")
-    #    self.assertEqual(len(all_users), 0)
 
+
+        #input1
+        # Checking for a short username
+        # self.assertEqual(resp.json()["msg"], "The username is less than 4 characters or more than 10 characters")
+
+        #input2
         # Checking for long username
-    #    self.assertEqual(resp.json()["msg"], "Username less than 4 characters or more than 16 characters")
-    #    self.assertEqual(len(all_users), 0)
+        # self.assertEqual(resp.json()["msg"], "The username is less than 4 characters or more than 10 characters")
 
-    #    Checking for a valid username
-        # self.assertEqual(resp.json()["msg"], "OK")
-        # self.assertEqual(len(all_users),2)
-
+        #input3
         # Checking for a short password
-    #    self.assertEqual(resp.json()["msg"], "Password less than 8 characters or more than 24 characters")
-    #    self.assertEqual(len(all_users), 1)
-
+        # self.assertEqual(resp.json()["msg"], "The password is less than 8 characters or more than 30 characters")
+  
+        #input4
         # Checking for a long password
-    #    self.assertEqual(resp.json()["msg"], "Password less than 8 characters or more than 24 characters")
-    #    self.assertEqual(len(all_users), 1)
+        # self.assertEqual(resp.json()["msg"], "The password is less than 8 characters or more than 30 characters")
 
-        # Checking for a valid password
-    #    self.assertEqual(resp.json()["msg"], "OK")
-    #    self.assertEqual(len(all_users), 3)
+        #input5
+        #Checking for a valid password
+        # self.assertEqual(resp.json()["msg"], "OK")
 
+        #input5 again
         #Checking for duplicate players
-        self.assertEqual(resp.json()["msg"], "Username already exists")
-        self.assertEqual(len(all_users), 2)
+        self.assertEqual(resp.json()["msg"], "The username already exists!")
