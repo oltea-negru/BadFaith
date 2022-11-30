776ccde1f5b6218a640d78a1b826e3d877f43eec
Oltea <oltea1907@gmail.com>
2022-11-30 18:04:04 +0000

    settings skeleton

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,4 +1,14 @@
-export default function Settings()
+export default function Settings(props)
 {
-    return (<div><p>Settings</p></div>);
+    return (<div className="grid place-items-center bg-orange-900 w-full h-screen">
+        <button className="absolute top-0 right-0 mt-5 mr-5 hover:bg-orange-400 outline bg-orange-800 text-white w-12 h-10">Edit</button>
+        <div className="grid place-items-center bg-orange-400 w-[80%] h-[70%]">
+
+            <img src={props.avatar} alt="player avatar" />
+            <p className="font-bloomberg text-2xl">{props.nickname}</p>
+            <p>{props.stats}</p>
+            <p>{props.friends}</p>
+        </div>
+
+    </div>);
 }
