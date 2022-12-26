06cd1a677c5103532519fec90d94c7934bfe16b3
Cameron Field <cf3g19@soton.ac.uk>
2022-12-26 19:54:21 +0000

    Functionality for chat almost complete, needs to pass in player nickname and then emit

--- frontend/src/pages/ChatTest.js
+++ frontend/src/pages/ChatTest.js
@@ -1,35 +1,46 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ChatMessage from "../components/chat";
+import '../components/Chat.css';
 let messages = new Array();
 let chatMessage;
 const defaultPlayer = "Default";
 export default function Chat() {
-    
+
     const [chatMessage, setMessage] = useState('');
     return (
         <div>
-            <div>
-
-                <input
+            <div className="chat" id="chatbox">
+                <ul>
+                    {messages.map((m) =>
+                        <ChatMessage message={m} />)}
+                </ul>
+            </div>
+            <div className="input">
+                <input className="chatBar"
+                    id="chatBar"
                     type="text"
                     value={chatMessage} placeholder="Chat"
-                    onChange={(e) => setMessage(e.target.value)} /> <button onClick={() => {
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyPress={(e) => {
+                        if(e.key === "Enter") {
+                            e.preventDefault();
+                            UpdateChat(chatMessage);
+                            setMessage('');
+                        }
+                    }} />
+                <button
+                    id="chatButton"
+                    onClick={() => {
                         UpdateChat(chatMessage)
                         setMessage('');
-                    }
-                    }>Enter</button>
+                    }}>Enter</button>
 
             </div>
-            <ul id="chat">
-                {messages.map((m) =>
-                    <ChatMessage message={m} />)}
-            </ul>
         </div>
     );
 }
 
 function UpdateChat(message) {
-    console.log('Message Added ' + message);
-    messages.push({player: defaultPlayer, message: message});
+    messages.push({ player: defaultPlayer, message: message });
 }
