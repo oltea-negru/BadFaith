b4454f4ead6ffe6c74c6665fec82fa02abb1fe7e
Oltea <oltea1907@gmail.com>
2022-12-30 06:33:48 +0000

    jumping players less horrid

--- frontend/src/components/player_waiting.js
+++ frontend/src/components/player_waiting.js
@@ -3,7 +3,7 @@ export default function PlayerWaiting(props)
     return (
         <div >
             <div style={{ "animation-duration": 0.2 * props.index + "s" }} class="logo">
-                <div style={{ "background-color": props.color }} className="text-white text-4xl ml-6 font-another">{props.text}</div>
+                <div style={{ "background-color": props.color }} className="text-white p-2 rounded-md text-4xl ml-3 font-another">{props.text}</div>
             </div>
         </div>
     );
