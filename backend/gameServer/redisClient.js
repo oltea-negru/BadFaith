229070081e098045d29774976409800f6afb5f76
cf3g19 <56079097+cf3g19@users.noreply.github.com>
2023-01-05 22:01:47 +0000

    Update redisClient.js

--- backend/gameServer/redisClient.js
+++ backend/gameServer/redisClient.js
@@ -6,24 +6,62 @@ bluebird.promisifyAll(redis);
 const redisHost = process.env.REDIS_HOST || 'localhost'
 const redisPort = process.env.REDIS_PORT || '6379'
 
-export default class HotStorageClient{
-    constructor(){
-        this.client = redis.createClient({ url: `redis://${redisHost}:${redisPort}`})
+export default class HotStorageClient {
+    constructor() {
+        this.client = redis.createClient({ url: `redis://${redisHost}:${redisPort}` })
     }
 
-    async connect(){
+    async connect() {
         await this.client.connect()
     }
 
-    async createLobbyDocument(lobbyCode){
+    async createLobbyDocument(lobbyCode) {
 
     }
 
-    async joinLobby(lobbyCode, hostDetails){
+    async joinLobby(lobbyCode, hostDetails) {
+        var lobbyDoc = await this.fetchLobbyDoc(lobbyCode)
+
+    }
+
+    async playerReady(lobbyCode, playerDetails) {
+        //fetchLobbyDocument
+        var lobbyDoc = await this.fetchLobbyDoc(lobbyCode)
+        lobbyDoc['ready']++;
+    }
+
+    async progressGameState(lobbyCode) {
+        var lobbyDoc = await this.fetchLobbyDoc(lobbyCode)
+    }
+
+    async playerAction(lobbyCode, actionDetails) {
+        var lobbyDoc = await this.fetchLobbyDoc(lobbyCode)
+        switch(actionDetails['action']){ //considering changing this to just map the input to the doc
+
+        }
+    }
+
+    async nextEvent(lobbyCode) {
+        var lobbyDoc = await this.fetchLobbyDoc(lobbyCode)
+        var event_array = lobbyDoc['events']
+        lobbyDoc['currentEvent']= event_array.shift()
+        lobbyDoc['events'] = event_array
         
     }
 
-    async doesLobbyExist(lobbyCode){
+    async doesLobbyExist(lobbyCode) {
+        var lobbyDoc = await this.fetchLobbyDoc(lobbyCode)
+        return lobbyDoc != null
+    }
+
+    async fetchLobbyDoc(lobbyCode) {
+        var lobbyDoc
+        //Fetch lobby doc associated with lobbycode
+        return lobbyDoc
+    }
+
+    async updateLobbyDoc(lobbyCode,lobbyDoc)
+    {
         
     }
 }
