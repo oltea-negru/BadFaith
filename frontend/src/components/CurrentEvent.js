9c8041ef61291668be5568494a722cac4cf31af1
Thanuj V <tvasuthevan123@gmail.com>
2023-01-13 02:16:37 +0000

    Hacking to get dummy data to event room

--- frontend/src/components/CurrentEvent.js
+++ frontend/src/components/CurrentEvent.js
@@ -1,11 +1,22 @@
 import EventMap, { OutsideEvent } from "./eventMap";
 import Evidence from "../assets/svg/EvidenceBoard.svg"
 import { useSelector } from "react-redux";
+import { EventGenMap } from "../components/eventMap";
 
 export function CurrentEvent() {
     const { lobby } = useSelector(state => state.game)
+    
+    const currEvent =  EventGenMap("Blackmailed", {
+        nickname: "LoremIpsum",
+        icon: "Figure this out",
+        original: "Enemy",
+        allegiance: "Enemy"
+    }, getPlayerArray())
 
-    let event = EventMap(lobby.currentEvent);
+    console.log('Curr', currEvent)
+
+    let event = EventMap(dummylobbyState);
+    console.log('Event', event)
     return (
         <div className="">
             <img src={Evidence} alt="Evidence Board" className="absolute right-[20%] top-[5%] h-1/2" />
@@ -20,3 +31,59 @@ export function EventWaiting() {
         <OutsideEvent />
     );
 }
+
+const inEvent = true
+const dummylobbyState = {
+    "inEvent": inEvent,
+    "id": "",
+    "players": {
+        "DummyID": {
+            nickname: "LoremIpsum",
+            icon: "Figure this out",
+            original: "Enemy",
+            allegiance: "Enemy"
+        },
+        "Lorem": {
+            nickname: "Sean Connery",
+            icon: "Figure this out",
+            original: "Enemy",
+            allegiance: "Ally"
+        },
+        "Ipsum": {
+            "nickname": "Travolta",
+            "icon": "Figure this out",
+            original: "Ally",
+            allegiance: "Enemy",
+            "target": "",
+        },
+        "Delta": {
+            nickname: "Geronimo",
+            original: "Ally",
+            allegiance: "Ally"
+        },
+        "Beta": {
+            nickname: "Jester",
+            original: "Enemy",
+            allegiance: "Enemy"
+        }
+    },
+    "remainingPlayers": ["Lorem", "Snorlax"],
+    "invited": [],
+    "host": "",
+    "code": "",
+    "events": [],
+    "state": 5,
+    "eventHistory": [],
+    "currentEvent": {}
+}
+function getPlayerArray()
+{
+    let playerArray = [];
+    Object.keys(dummylobbyState.players).forEach(player =>
+    {
+        playerArray.push(dummylobbyState.players[player]);
+    })
+    console.log('Players', playerArray)
+    return playerArray;
+}
+
--- frontend/src/pages/EventRoom.js
+++ frontend/src/pages/EventRoom.js
@@ -4,22 +4,23 @@ import { useSelector } from "react-redux";
 export default function EventRoom() {
     const { lobby, player } = useSelector(state => state.game);
 
-    if (lobby.state == 5) {
-        if (lobby.currentEvent.player.socketID == player.socketID) {
-            console.log('InEvent')
+    // if (lobby.state == 5) {
+    //     if (lobby.currentEvent.player.socketID == player.socketID) {
+    //         console.log('InEvent')
             return (
                 <div className=" bg-event_room h-screen bg-cover bg-bottom">
                     <CurrentEvent />
                 </div>
             )
-        } else {
-            console.log('OutEvent')
-            return (
-                <div>
-                    <EventWaiting />
-                </div>
+        // } else {
+        //     console.log('OutEvent')
+        //     return (
+        //         <div>
+        //             <EventWaiting />
+        //         </div>
 
-            )
-        }
-    }
+        //     )
+        // }
+    // }
 }
+
